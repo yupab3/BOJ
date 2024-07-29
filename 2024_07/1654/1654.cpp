dd1192c48c31aeff55b29a387047892fd93ad470
KDY <39092976+yupab3@users.noreply.github.com>
2024-07-29 15:39:04 +0000

    home

--- 2024_07/1654/1654.cpp
+++ 2024_07/1654/1654.cpp
@@ -1,1 +1,37 @@
-today
+#include <iostream>
+
+using namespace std;
+
+size_t arr[10005];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    int k, n;
+    cin >> k >> n;
+    size_t cnt, ans, m_len = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        cin >> arr[i];
+        m_len = max(arr[i], m_len);
+    }
+    size_t left = 1, right = m_len + 1;
+    while (1)
+    {
+        cnt = 0;
+        ans = (right + left) / 2;
+        for (int i = 1; i <= k; i++)
+        {
+            cnt += arr[i] / ans;
+        }
+        if (left == right - 1)
+            break ;
+        else if (cnt >= n)
+            left = ans;
+        else if (cnt < n)
+            right = ans;
+    }
+    cout << ans << '\n';
+}
--- 2024_07/3079/3079.cpp
+++ 2024_07/3079/3079.cpp
@@ -2,15 +2,37 @@
 
 using namespace std;
 
-ssize_t t[100005];
+size_t t[100005];
 int	main()
 {
 	ios::sync_with_stdio(0);
 	std::cin.tie(0);
 
-	ssize_t n, m, tmp;
+	size_t n, m, mid, cnt;
+	size_t t_max = 0;
 	cin >> n >> m;
-	for (ssize_t i = 1; i <= n; i++)
+	for (size_t i = 1; i <= n; i++)
+	{
 		cin >> t[i];
-	
+		t_max = max(t[i], t_max);
+	}
+	size_t left = 1, right = -1;
+	while(1)
+	{
+		mid = (left + right) / 2;
+		cnt = 0;
+		for (size_t i = 1; i <= n; i++)
+		{
+			cnt += mid / t[i];
+			if (cnt > m)
+				break ;
+		}
+		if (left == right - 1)
+			break;
+		else if (cnt >= m)
+			right = mid;
+		else if (cnt < m)
+			left = mid;
+	}
+	cout << right << '\n';
 }
